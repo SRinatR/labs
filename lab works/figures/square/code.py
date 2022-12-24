8041477dd6654f4af44666a1578895530672d128
Rinat <46104016+SRinatR@users.noreply.github.com>
2022-12-24 16:52:03 +0500

    Add files via upload

---
+++ lab works/figures/square/code.py
@@ -0,0 +1,10 @@
+import math
+__a = 15
+def  square_perimeter(a=__a):
+    P = a*4
+    print(P)
+def square_area(a=__a):
+    S = a^2 
+    print(S)
+    
+    
---
+++ lab works/figures/triangle/code.py
@@ -0,0 +1,14 @@
+import math
+__a = 7
+__b = 2
+__c = 8
+def triangle_perimeter(a=__a,b=__b,c=__c):
+    P = a + b + c
+    print(P)
+triangle_perimeter()
+def triangle_area(a=__a,b=__b,c=__c):
+    p = (__a+__b+__c)/2
+    A = math.sqrt(p(p-a)*(p-b)*(p-c))
+    print(A)
+triangle_area()
+
